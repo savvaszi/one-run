d2ba70e7009797e31c442c3bf93647423cfa9d8b
One Run Dev <dev@onerun.local>
2026-05-20 07:21:47 +0300

    fix: use set:html for icons, fix astro config for v5 output:static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: node({ mode: 'standalone' }),
   server: { port: 4321 },
   vite: {
